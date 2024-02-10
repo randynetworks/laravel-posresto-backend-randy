5ccfdd6cb31c9577b7f1171b2c9117641d4a5286
randy <randy.ramadhan@bts.id>
2024-02-10 13:23:40 +0700

    implement authentication

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,9 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'superadmin',
+            'email' => 'superadmin@test.dev',
+            'password' => \Hash::make('12345678')
+        ]);
     }
 }
--- resources/views/template/dashboard-general-dashboard.blade.php
+++ resources/views/template/dashboard-general-dashboard.blade.php
@@ -14,7 +14,7 @@
     <div class="main-content">
         <section class="section">
             <div class="section-header">
-                <h1>Dashboard</h1>
+                <h1>Dashboard - Randy Ramadhan</h1>
             </div>
             <div class="row">
                 <div class="col-lg-3 col-md-6 col-sm-6 col-12">
